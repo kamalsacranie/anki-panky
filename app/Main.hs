73cf967da403dd60dd1bd9eca862b86abc6891c6
Kamal Sacranie <kamalsjunkmail@gmail.com>
2024-02-06 12:23:57 +0000

    Partiallly add cli options

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,15 @@
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 import Collection.Generate
 import Constructor (produceDeck)
-import Control.Monad.State.Lazy (StateT (runStateT))
-import Data.Maybe (catMaybes)
+import Control.Monad.State.Lazy (StateT (runStateT), when)
 import Data.Text (pack)
-import Data.Text.IO qualified as T (readFile)
+import Data.Text.Lazy qualified as LT (takeWhile, toStrict, unpack)
+import Data.Text.Lazy.IO qualified as LTO (readFile)
 import System.Directory (makeAbsolute)
 import System.Environment (getArgs)
 import System.FilePath (takeBaseName)
 import Types.Anki
-
-data Opt
-  = PlacehodlerOpt
-  deriving (Show)
-
-data PankyArg
-  = File FilePath
-  | Opt Opt
-  deriving (Show)
-
-parseOpt :: String -> Maybe PankyArg
-parseOpt _ = Nothing
 
 handleFile :: FilePath -> IO ()
 handleFile file = do
@@ -34,23 +21,70 @@ handleFile file = do
             deckId = Nothing,
             mediaDG = []
           }
-  input <- T.readFile (filePath genInfo)
-  (renderedCards, genInfoPostProd) <- runStateT (produceDeck input) genInfo
+  input <- LTO.readFile (filePath genInfo)
+  let firstLine = LT.takeWhile (/= '\n') input
+  let isValidFile = case LT.unpack firstLine of
+        "---" -> True
+        ('#' : ' ' : _) -> True
+        _ -> False
+  when isValidFile $ do
+    (renderedCards, genInfoPostProd) <- runStateT (produceDeck (LT.toStrict input)) genInfo
+    if null renderedCards
+      then putStrLn $ "Skipping file \"" ++ deckFileName genInfoPostProd ++ "\" as it failed to parse"
+      else generateCollection genInfoPostProd renderedCards
 
-  generateCollection genInfoPostProd renderedCards
+splitStringOnce :: Char -> String -> [String]
+splitStringOnce _ "" = []
+splitStringOnce delimiter str =
+  let (first, remainder) = break (== delimiter) str
+   in first : case remainder of
+        (_ : rest) -> [rest]
+        [] -> []
 
 main :: IO ()
 main = do
-  maybeArgs <-
-    mapM
-      ( \case
-          ('-' : '-' : opt) -> pure (parseOpt opt)
-          ('-' : shortOpt) -> pure (parseOpt shortOpt)
-          file -> Just <$> (File <$> makeAbsolute file)
-      )
-      <$> getArgs
-  args <- catMaybes <$> maybeArgs
+  rawArgs <- concatMap (splitStringOnce '=') <$> getArgs
+  let args = parseArgs rawArgs
 
   let inputFiles = [file | File file <- args]
-  let _ = [opt | Opt opt <- args]
-  mapM_ handleFile inputFiles
+  absInputFiles <- mapM makeAbsolute inputFiles
+  let _opts = [arg | arg <- args, (case arg of File _ -> False; _ -> True)]
+  mapM_ handleFile absInputFiles
+
+data PankyFlag
+  = Verbose
+  deriving (Show)
+
+data PankyKWarg
+  = DeckName
+  deriving (Show)
+
+data PankyOption
+  = Flag PankyFlag
+  | Opt PankyKWarg
+  deriving (Show)
+
+data PankyArg
+  = File FilePath
+  | PFlag PankyFlag
+  | POpt PankyKWarg String
+  deriving (Show)
+
+parsePankyOption :: String -> Maybe PankyOption
+parsePankyOption "V" = return $ Flag Verbose
+parsePankyOption "-verbose" = return $ Flag Verbose
+parsePankyOption "-name" = return $ Opt DeckName
+parsePankyOption _ = Nothing
+
+parseArgs :: [String] -> [PankyArg]
+parseArgs [] = []
+parseArgs ['-' : optString] = case parsePankyOption optString of
+  Just (Flag flag) -> [PFlag flag]
+  Just (Opt _) -> error "Option without value"
+  Nothing -> error $ "Invalid CLI arg -" ++ optString
+parseArgs [x] = [File x]
+parseArgs (('-' : optString) : optv : xs) = case parsePankyOption optString of
+  Just (Flag flag) -> PFlag flag : parseArgs (optv : xs)
+  Just (Opt kwarg) -> POpt kwarg optv : parseArgs xs
+  Nothing -> error $ "Invalid CLI arg -" ++ optString
+parseArgs (file : xs) = File file : parseArgs xs
