0e9481b545f0b0258ac2b4fc96a71c417cdc64d6
Kamal Sacranie <kamalsjunkmail@gmail.com>
2024-02-24 17:34:20 +0000

    Update homebrew formula

--- anki-panky.rb
+++ anki-panky.rb
@@ -1,9 +1,15 @@
 class AnkiPanky < Formula
   desc "Pandoc Markdown Anki collection compiler for generating Anki collections from Pandoc-style Markdown files"
   homepage "https://github.com/kamalsacranie/anki-panky"
-  url "https://github.com/kamalsacranie/anki-panky/releases/download/0.0.0.1/anki-panky-0.0.0.1-x86_64-macos.tar.gz"
-  sha256 "725be26e5eab0c3bf417cb368717e82ddd11cd422a161307163541bb4fc3029f"
-  license "GPL-3.0"
+  url ""
+  if Hardware::CPU.arm?
+    url "https://example.com/package-arm.tar.gz"
+    sha256 "..."
+  else
+    url "https://github.com/kamalsacranie/anki-panky/releases/download/0.0.0.1/anki-panky-0.0.0.1-x86_64-macos.tar.gz"
+    sha256 "725be26e5eab0c3bf417cb368717e82ddd11cd422a161307163541bb4fc3029f"
+  end
+  license "GPL-3.0-or-later"
 
   def install
     prefix.install Dir["*"]
