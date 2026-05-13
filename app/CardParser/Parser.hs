9f0bb9b03f3d5f546c2cd5635832c1d961b88903
Kamal Sacranie <kamalsjunkmail@gmail.com>
2026-05-13 20:35:58 +0100

    Fail hard on parsing error and allow blocks to have trailing horizontal rules

--- app/CardParser/Parser.hs
+++ app/CardParser/Parser.hs
@@ -3,11 +3,11 @@ module CardParser.Parser (parseCards) where
 import CardParser.ExtCard (extendedCard)
 import CardParser.SimpleCard (simpleCard)
 import Control.Applicative (Alternative (many), (<|>))
-import Text.Pandoc (Block)
+import Text.Pandoc (Block, Block(HorizontalRule))
 import Types.Parser
 
 parseAll :: Parser a -> [Block] -> [a]
-parseAll p bs = [a | (a, []) <- parse p bs]
+parseAll p bs = [a | (a, rest) <- parse p bs, all (== HorizontalRule) rest]
 
 cards :: Parser [Card]
 cards = many (extendedCard <|> simpleCard)
